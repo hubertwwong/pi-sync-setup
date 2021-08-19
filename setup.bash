5f4c09472949471dad078e215dc168347d1eb911
Hubert Wong <hubertwwong@gmail.com>
2021-08-18 23:15:38 -0700

    Repo

--- setup.bash
+++ setup.bash
@@ -4,3 +4,7 @@ echo "> Initial setup. Install required libraries."
 sudo apt-get update
 sudo apt-get install -y git vim python3 python3-pip
 pip3 install ansible
+
+echo "> Clone repo"
+rm -rf /tmp/pi-sync-setup
+git clone git@github.com:hubertwwong/pi-sync-setup.git
